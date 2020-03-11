692f276bbe946b324b891e661321a044d1034115
funfunhada <kjhkjh0929@gmail.com>
2020-03-11 17:40:47 +0900

    JpaAuditing, HelloControllerTest 관련 테스트 오류 수정

--- src/test/java/com/funfunhada/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/funfunhada/book/springboot/web/HelloControllerTest.java
@@ -1,10 +1,14 @@
 package com.funfunhada.book.springboot.web;
 
+import org.apache.catalina.security.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -12,7 +16,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@WebAppConfiguration
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        })
 public class HelloControllerTest {
 
     @Autowired
