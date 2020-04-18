924f1f1614ed562b41ae0007e6c0b5bc41bbb467
funfunhada <kjhkjh0929@gmail.com>
2020-04-18 22:04:11 +0900

    10, switch.sh

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+
 ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
-source ${ABSDIR}/profile.sh
+source ${ABSDIR/profile.sh}
+
 
 function switch_proxy() {
-    IDLE_PORT=$(find_idle_prot)
+    IDLE_PORT=$(find_idle_port)
 
     echo "> 전환할 Port: $IDLE_PORT"
     echo "> Port 전환"
